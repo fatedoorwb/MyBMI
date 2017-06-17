8c1b3a3ccc298c55456a323789fb38e3bd735edb
kuchu13 <105021033@live.asia.edu.tw>
2017-06-18 00:32:13 +0800

    NewDataPanel二點顛(??

--- src/idv/kuchu/mybmi/component/NewDataPanel.java
+++ src/idv/kuchu/mybmi/component/NewDataPanel.java
@@ -6,6 +6,60 @@ import java.text.ParseException;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import idv.kuchu.mybmi.MainScreen;
+
+public class NewDataPanel extends Panel{
+
+	Font font = new Font("微軟正黑體", Font.BOLD, 28);
+
+
+	int X = ((MainScreen.SCREEN_W-16)-400)/2-8;
+	int Y = 32;
+	
+	int BX = ((MainScreen.SCREEN_W-16)-200)/2-8;
+	int BY = ((MainScreen.SCREEN_H-38)-64)-50;
+
+	public NewDataPanel() {
+		
+		JLabel  lableHeight = new JLabel ("身高");
+		lableHeight.setFont(font);
+		lableHeight.setBounds(X, Y, 64, 64);
+		JTextField  textboxHeight = new JTextField();
+		textboxHeight.setBounds(100+X, 100+Y, 300, 64);
+		textboxHeight.setFont(font);
+		
+		JLabel  lableWeight = new JLabel ("體重");
+		lableWeight.setFont(font);
+		lableWeight.setBounds(X, Y, 64, 64);
+		JTextField  textboxWeight = new JTextField();
+		textboxWeight.setBounds(100+X, 100+Y, 300, 64);
+		textboxWeight.setFont(font);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		this.add(lableHeight);
+		this.add(lableWeight);
+
+		
+		
+		
+		
+
 		
 	}
 	
