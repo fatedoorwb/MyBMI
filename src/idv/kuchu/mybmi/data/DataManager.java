1bdf65123288016e385dde62c08851a453ab716f
fatedoorwb <fatedoor.wb@gmail.com>
2017-06-17 21:45:40 +0800

    讀寫檔前置

--- src/idv/kuchu/mybmi/data/DataManager.java
+++ src/idv/kuchu/mybmi/data/DataManager.java
@@ -3,5 +3,12 @@ package idv.kuchu.mybmi.data;
 public class DataManager {
 	
 	public final static DataManager instance = new DataManager();
+	
+	private DataManager(){
+	}
+	
+	public boolean getUserData(){
+		return false;
+	}
 
 }
