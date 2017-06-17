6dc212244d9c7449c36b54a22194494d5fd206a3
fatedoorwb <fatedoor.wb@gmail.com>
2017-06-18 02:17:13 +0800

    Null

--- src/idv/kuchu/mybmi/component/NewDataPanel.java
+++ src/idv/kuchu/mybmi/component/NewDataPanel.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import idv.kuchu.mybmi.MainScreen;
@@ -108,8 +109,13 @@ public class NewDataPanel extends Panel {
 					int day = 1;
 				}
 				JSONObject user = DataManager.instance.getUserData();
-				MainScreen.getInstance()
-						.add(new DataAnalyzePanel(user.getInt("gender"), 2017 - user.getInt("year"), height, weight));
+				try {
+					MainScreen.getInstance()
+							.add(new DataAnalyzePanel(user.getInt("gender"), 2017 - user.getInt("year"), height, weight));
+				} catch (JSONException exception) {
+					JOptionPane.showMessageDialog(null, exception.toString());
+					return;
+				}
 			}
 
 		});
